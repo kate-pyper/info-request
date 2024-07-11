80ad9c1c1f41e42de2eff63ba3bc42be0aa31d2e
kate-pyper <kate.pyper@phs.scot>
2024-07-11 16:14:10 +0100

    add function to create new file

---
+++ R/add_script.R
@@ -0,0 +1,43 @@
+add_script <- function(name, script_folder = TRUE){
+  script_temp <- "##########################################################
+# Name of file
+# Data release (if applicable)
+# Original author(s)
+# Original date
+# Latest update author (if not using version control)
+# Latest update date (if not using version control)
+# Latest update description (if not using version control)
+# Type of script (e.g. extraction, preparation, modelling)
+# Written/run on (e.g. R Studio SERVER)
+# Version of R that the script was most recently run on
+# Description of content
+# Approximate run time
+##########################################################
+
+
+### 1 - Housekeeping ----
+# This section should be the only section of the script which requires manual changes 
+# for future updates and includes:
+#   loading packages
+#   setting filepaths and extract dates
+#   functions (defined here or sourced from another file)
+#   setting plot parameter
+#   specifying codes (e.g. ICD-10 codes)
+
+
+### 2 Section Heading ----
+
+
+### 3 Section Heading ----
+
+
+### END OF SCRIPT ###"
+  
+  if(script_folder){
+    writeLines(script_temp, con = file.path("3. Scripts", name))
+    file.edit(file.path("3. Scripts", name))
+  } else {
+    writeLines(script_temp, con = paste0(name))
+    file.edit(file.path(name))
+  }
+}
